99bd30d7763c30f18d25d8daa58489a4ca2c15e6
Renan Rafael Storrer <storrerrenan@gmail.com>
2023-06-27 01:46:39 -0300

    feat: implementa função para aparecer tarefas

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
+  function showData() {
+    $.ajax ({
+      url: 'show.php',
+      type: 'post',
+      success: function(result){
+        $("#data").html(result);
+      }
+    })
+  }
+  showData()
+
   $("#btn-add").on("click", function(e){
     e.preventDefault();
     txt = $("#txt").val();
@@ -11,6 +22,7 @@ $(document).ready(function(){
         if (result == 1) {
           txt = $("#txt").val('');
           alert("Tarefa adicionada com sucesso.");
+          showData()
         }else {
           console.log(result);
         }
