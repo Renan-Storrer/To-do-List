055694f0789dd68fc0e600a2c5f5448cebe19543
Renan Rafael Storrer <storrerrenan@gmail.com>
2023-06-27 01:36:23 -0300

    feat: implementa conexão entre input com o banco de dados

--- insert.php
+++ insert.php
@@ -1,8 +1,15 @@
-<?php 
-    include 'config.php';
+<?php
+  // Include Connection
+  include 'config.php';
 
-    $txt = $_POST['$txt'];
+  $txt = $_POST['txt'];
 
-    $sql = "INSERT INTO demo (txt) VALUES('$txt')";
-    
+  $sql = "INSERT INTO demo (txt) VALUES ('$txt')";
+  $result = mysqli_query($conn, $sql);
+
+  if ($result) {
+    echo 1;
+  }else {
+    echo "Error: {$sql}" . mysqli_error($conn);
+  }
 ?>
--- script.js
+++ script.js
@@ -2,13 +2,18 @@ $(document).ready(function(){
   $("#btn-add").on("click", function(e){
     e.preventDefault();
     txt = $("#txt").val();
-    
+
     $.ajax({
       url: 'insert.php',
       type: 'post',
-      data: '{txt: txt}',
-      success: function(restult) {
-        
+      data: {txt: txt},
+      success: function(result){
+        if (result == 1) {
+          txt = $("#txt").val('');
+          alert("Tarefa adicionada com sucesso.");
+        }else {
+          console.log(result);
+        }
       }
     })
   })
